f50b15ca7a1e603139fe8e490e27e041b69a5e2a
Heinrich <heinrichep@grabmayr.net>
2022-09-23 17:09:26 +0200

    debugs

--- FlowAcquisition.py
+++ FlowAcquisition.py
@@ -12,38 +12,170 @@
 import logging
 from icecream import ic
 
-from pycromanager import Acquisition, multi_d_acquisition_events, start_headless
+from pycromanager import Acquisition, multi_d_acquisition_events, start_headless, Core
 # import monet.control as mcont
 from arduino_connection import AriaTrigger
+import time
 
 
 logger = logging.getLogger(__name__)
 ic.configureOutput(outputFunction=logger.debug)
 
 
-mm_app_path = '/path/to/micromanager'
-config_file = mm_app_path + "/MMConfig_demo.cfg"
+mm_app_path = r'C:\Program Files\Micro-Manager-2.0'
+config_file = r'C:\Users\miblab\Desktop\MMConfig_1.cfg'
 
-save_dir = r"C:\Users\henry\Desktop\data"
+save_dir = r"Z:\users\grabmayr\FlowAutomation\testdata"
 base_name = 'exchange_experiment'
 n_rounds = 4
-n_frames = 30000
+n_frames = 10
 t_exp = .1
+aria_timeout = 30*60
 # laser = 561
 # laser_power = 35
 
+from pathlib import Path
+from time import sleep
+from functools import partial
+import gc
+
+# this function is executed once the acquisition engine has setup
+def post_hook_fn(event,bridge,event_queue):
+
+    # open bridge to MM
+    bridge = Bridge()
+    core = bridge.get_core()
+
+    return event
+
+# this function makes sure that all data is written to disk
+def storage_monitor_callback_fn(final_z,final_c,final_e,axes):
+
+    global data_storage_finished
+
+    print(axes['z'],axes['c'],axes['e'])
+
+    if axes['z']==(final_z-1) and axes['c']==(final_c-1) and axes['e']==(final_e-1):
+        print('Setting flag, writing last image.')
+        data_storage_finished = True
+
+def test_acq3():
+
+    from pycromanager import __version__
+    print('pycromanager', __version__)
+
+    from zmq import __version__
+    print('zmq', __version__)
+
+    core = Core()
+
+    events = multi_d_acquisition_events(num_time_points=n_frames)
+
+    # Loop over N acquisitions
+    for i in range(4):
+        print (f'\nAcquisition {i}')
+        time.sleep(10)
+
+        try:
+            with Acquisition(directory=save_dir, name='test', show_display=False, debug=False) as acq:
+                acq.acquire(events)
+
+                del acq       # Adding ths didn't help
+                gc.collect()  # Adding ths didn't help
+                print('\tAll good!')
+
+        except Exception as e:
+            print(e)
+
+
+def test_acq2():
+    start_headless(mm_app_path, config_file, timeout=5000)
+
+    print('started headless')
+
+    acq_name = r'test_acquisition'
+
+    # global data_storage_finished
+    # # construct partial functions for acquisition hooks
+    # storage_monitor_callback_metadata_fn = partial(storage_monitor_callback_fn, n_frames, 1, 1)
+    #
+    # data_storage_finished = False
+
+    with Acquisition(
+            directory=save_dir, name=acq_name, show_display=False,
+                     ) as acq:
+        print('creating events')
+        events = multi_d_acquisition_events(num_time_points=n_frames)
+        print('printing events')
+        for e in events:
+            print(e)
+        print('acquiring')
+        acq.acquire(events)
+
+    # # wait until all data is written to disk
+    # while not(data_storage_finished):
+    #     sleep(1)
+    #     print('Storage not finished')
+
+    # clean up this acquisition object and force Python to run garbage collector
+    acq = None
+    gc.collect()
+
+
+    print('second run')
+
+    acq_name = r'test_acquisition2'
+
+    with Acquisition(directory=save_dir, name=acq_name, show_display=False,
+                     ) as acq:
+        print('creating events')
+        events = multi_d_acquisition_events(num_time_points=n_frames)
+        print('printing events')
+        for e in events:
+            print(e)
+        print('acquiring')
+        acq.acquire(events)
+
+
+def test_acq():
+    start_headless(mm_app_path, config_file, timeout=5000)
+
+    print('started headless')
+
+    acq_name = r'test_acquisition'
+
+    with Acquisition(directory=save_dir, name=acq_name, show_display=False,
+                     ) as acq:
+        print('creating events')
+        events = multi_d_acquisition_events(
+            num_time_points=n_frames,
+            time_interval_s=t_exp,
+            channel_group='Filter turret', channels=['2-G561'],
+            channel_exposures_ms= [t_exp],
+        )
+        print('printing events')
+        for e in events:
+            print(e)
+        print('acquiring')
+        acq.acquire(events)
 
 def main():
     # Start the Java process
-    start_headless(mm_app_path, config_file, timeout=5000)
+    # start_headless(mm_app_path, config_file, timeout=5000)
+    core = Core()
+
+    print('started headless')
 
     # start aria triggering connection
     aria = AriaTrigger()
+    print('initialized triggering')
 
-    for round in n_rounds:
+    for round in range(n_rounds):
         acq_name = base_name + '_{:d}'.format(round)
 
-        aria.sense_pulse()
+        print('waiting for Aria pulsing to signal readiness for round {:d}'.format(round))
+        aria.sense_pulse(timeout=aria_timeout)
+        print('received pulse, now starting acquisition.')
         record_movie(save_dir, acq_name, n_frames, t_exp)
 
         print('Acquisition of ', acq_name, 'done.')
@@ -62,7 +194,7 @@ def record_movie(acq_dir, acq_name, n_frames, t_exp):
         events = multi_d_acquisition_events(
             num_time_points=n_frames,
             time_interval_s=t_exp,
-            channel_group='Channel', channels=['Cy3B'],
+            channel_group='Filter turret', channels=['2-G561'],
             channel_exposures_ms= [t_exp],
         )
         for e in events:
--- arduino_connection.py
+++ arduino_connection.py
@@ -16,33 +16,97 @@ from Arduino import Arduino
 
 
 class AriaTrigger():
-    def __init__(self, pulse_pin=13, pulse_duration=.2):
-        self.pulse_pin = 13
+    def __init__(self, pulse_pin=13, sense_pin=12, pulse_duration=.2):
+        self.pulse_pin = pulse_pin
+        self.sense_pin = sense_pin
         self.pulse_duration = pulse_duration
 
         self.board = Arduino()
-        self.board.pinMode(pulse_pin, "OUTPUT")
+        self.board.pinMode(self.pulse_pin, "OUTPUT")
         self.board.digitalWrite(self.pulse_pin, "LOW")
+        self.board.pinMode(self.sense_pin, "INPUT")
 
     def send_pulse(self):
-        self.board.pinMode(pulse_pin, "OUTPUT")
+        self.board.pinMode(self.pulse_pin, "OUTPUT")
         self.board.digitalWrite(self.pulse_pin, "HIGH")
         time.sleep(self.pulse_duration)
         self.board.digitalWrite(self.pulse_pin, "LOW")
 
-    def sense_pulse(self, timeout=10):
-        self.board.pinMode(pulse_pin, "INPUT")
+    def sense_pulse(self, timeout=10, baseline=False, refresh_rate=.01,
+                    min_duration=.05, max_duration=.6):
+        """
+        Args:
+            timeout : int
+                timeout in seconds
+            baseline : bool
+                TTL level of baseline (0V, with upward pulse,
+                    or 5V with downward pulse)
+            refresh_rate : float
+                refresh rate of pin sensing
+            min_duration : float
+                minimum duration of pulse
+        Returns:
+            triggered : bool
+                whether the pulse was detected
+        """
+        # find first edge
+        if baseline:
+            edge = 'falling'
+        else:
+            edge = 'rising'
+        triggered, edge_detected = self.sense_edge(timeout, edge, refresh_rate)
+        if not triggered:
+            # timeout
+            return False
+        tic = time.time()
+        # find second edge
+        if baseline:
+            edge = 'rising'
+        else:
+            edge = 'falling'
+        while True:
+            triggered, edge_detected = self.sense_edge(max_duration, edge, refresh_rate)
+            toc = time.time()
+            if toc-tic < min_duration:
+                # this was a mismeasurement; wait for the 'real' second edge
+                # of course also the first edge could have been mismeasured
+                continue
+            elif toc-tic >= min_duration and toc-tic <= max_duration:
+                return True
+            elif toc-tic > max_duration:
+                return False
+
+    def sense_edge(self, timeout=10, edge='rising', refresh_rate=.01):
+        """
+        Args:
+            timeout : int
+                timeout in seconds
+            edge : str
+                which edge to detect: one of rising, falling, both
+        """
+        self.board.pinMode(self.sense_pin, "INPUT")
         triggered = False
         tic = time.time()
+        previous_state = self.board.digitalRead(self.sense_pin)
         while not triggered:
-            triggered = self.board.digitalRead(self.pulse_pin)
+            state = self.board.digitalRead(self.sense_pin)
+            if edge=='rising' or edge=='both':
+                if (not previous_state) and state:
+                    triggered = True
+                    edge_detected = 'rising'
+            if edge=='falling' or edge=='both':
+                if previous_state and (not state):
+                    triggered = True
+                    edge_detected = 'falling'
             if triggered:
                 break
             if time.time()-tic > timeout:
                 print('Sensing TTL pulse timed out after {:.1f}s.'.format(timeout))
+                edge_detected = 'none'
                 break
-            time.sleep(.05)
-        return triggered
+            time.sleep(refresh_rate)
+            previous_state = state
+        return triggered, edge_detected
 
 
     def close(self):
